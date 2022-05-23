20495264462f2ce3454ea03f435f2c53cc806147
nrootconauto <nrootconauto@gmail.com>
2022-05-23 09:31:04 -0700

    Compiler is now more orthodox to TempleOS.
    I basically copied over the TempleOS compiler to get the error messages to fit more in line with the DolDoc engine.

--- Debian_pkg.sh
+++ Debian_pkg.sh
@@ -4,18 +4,24 @@ mkdir -p ${STAGEDIR}
 mkdir -p ${STAGEDIR}/usr
 mkdir -p ${STAGEDIR}/usr/local
 mkdir -p ${STAGEDIR}/usr/local/bin
-mkdir -p ${STAGEDIR}/usr/local/include/HolyC
+mkdir -p ${STAGEDIR}/usr/local/include/3Days
 cp 3d_loader ${STAGEDIR}/usr/local/bin/3d
 strip ${STAGEDIR}/usr/local/bin/3d
 chmod +x ${STAGEDIR}/usr/local/bin/3d
 set files = ( \
     HCRT.BIN \
+    `find T ` \
     )
 rm ${STAGEDIR}/plist
 echo "/usr/local/bin/3d" >> ${STAGEDIR}/plist
 
 foreach f ($files)
-    cp  HCRT/$f  ${STAGEDIR}/usr/local/include/HolyC/
+    if -d $f then
+        mkdir -p ${STAGEDIR}/usr/local/include/3Days/$f
+    else
+        cp  $f  ${STAGEDIR}/usr/local/include/3Days/
+	echo "/usr/local/include/3Days/$f" >> ${STAGEDIR}/plist
+    endif
 end
 
 dpkg-deb --build debian 3days.deb
